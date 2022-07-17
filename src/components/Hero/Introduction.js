bc1290f7857fb1fcc1c9cd09d06b0ea10c30b689
Shameek Kumar Baranwal <shameekbaranwal@gmail.com>
2022-07-17 20:43:33 +0530

    Updated Hero section layout.

--- src/components/Hero/Introduction.js
+++ src/components/Hero/Introduction.js
@@ -6,24 +6,24 @@ import Quotes from '../../assets/icons/quotes.svg';
 
 export default function Introduction() {
 	return (
-		<div className='flex justify-center items-center flex-col'>
+		<div className='flex flex-col items-center justify-center'>
 			<img
 				src={Quotes}
 				alt='quotes'
-				className='w-12 md:w-20 self-start lg:mt-12 -ml-4 sm:-ml-8 md:-ml-10'
+				className='relative self-start w-12 top-5 -left-10 lg:top-6 lg:-left-12'
 			/>
-			<p className='font-kruti-bold-italic text-3xl lg:text-2xl text-center lg:text-justify w-[22rem] md:w-[40rem] lg:w-[25rem]'>
+			<p className='font-kruti-bold-italic text-3xl lg:text-3xl text-center lg:text-justify w-[22rem] md:w-[40rem] lg:w-[25rem]'>
 				{Hindi.Introduction}
 			</p>
 			<img
 				src={Quotes}
 				alt='quotes'
-				className='rotate-180 w-12 md:w-20 lg:w-16 self-end -mr-4 sm:-mr-8 md:-mr-10 '
+				className='relative self-end w-12 rotate-180 -right-8 -top-8 md:-top-12 md:-right-6 lg:-right-12 lg:-top-10'
 			/>
 			<img
 				src={Signature}
 				alt='anjesh'
-				className='-mt-24 self-end w-48 md:w-auto'
+				className='self-end w-48 -mt-24 md:w-auto'
 			/>
 		</div>
 	);
--- src/components/NavBar/NavBrand.js
+++ src/components/NavBar/NavBrand.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default function NavBrand() {
 	return (
-		<h1 className='font-cinzel tracking-max'>anjesh&nbsp;&nbsp;baranwal</h1>
+		<a href='./#'>
+			<h1 className='font-cinzel tracking-max'>
+				anjesh&nbsp;&nbsp;baranwal
+			</h1>
+		</a>
 	);
 }
