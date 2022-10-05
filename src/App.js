d9e8b92e080b989245a754158b39c39b67ce213f
Shameek Kumar Baranwal <shameekbaranwal@gmail.com>
2022-10-05 19:29:11 +0530

    Implemented some redirects.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
@@ -11,6 +12,19 @@ import Reviews from './components/Reviews/index.js';
 import Footer from './components/Footer/index.js';
 
 const App = () => {
+	return (
+		<Routes>
+			<Route path='/' element={<MainAppComponent />}></Route>
+			<Route path='/buy' element={<BuyRedirect />} />
+			<Route path='/facebook' element={<FacebookRedirect />} />
+			<Route path='/twitter' element={<TwitterRedirect />} />
+			<Route path='/contact' element={<GmailRedirect />} />
+			<Route path='*' element={<Navigate to='/' />} />
+		</Routes>
+	);
+};
+
+const MainAppComponent = () => {
 	const [show, setShow] = useState(true);
 	return (
 		<div className='w-screen h-full overflow-x-hidden max-w-[100%]'>
@@ -28,5 +42,19 @@ const App = () => {
 		</div>
 	);
 };
+
+const BuyRedirect = () =>
+	window.location.replace(
+		'https://garudabooks.com/sangachchhadhvam-sindhu-tat-ka-anaam-gaaon',
+	);
+
+const FacebookRedirect = () =>
+	window.location.replace('https://facebook.com/anjeshbnl');
+
+const GmailRedirect = () =>
+	window.location.replace('mailto:anjeshbaranwal@gmail.com');
+
+const TwitterRedirect = () =>
+	window.location.replace('https://twitter.com/anjeshbaranwal');
 
 export default App;
