d3288a341ca535bf38f9778ee8d20ece5080ddc7
Shameek Kumar Baranwal <shameekbaranwal@gmail.com>
2022-11-06 17:23:26 +0530

    Updated twitter feed look.

--- src/components/TwitterFeed/index.jsx
+++ src/components/TwitterFeed/index.jsx
@@ -5,13 +5,15 @@ import { TwitterTimelineEmbed } from 'react-twitter-embed';
 export default function index() {
 	return (
 		<div className='bg-platinum bg-opacity-[80%] w-full flex justify-center items-center md:bg-opacity-100'>
-			<div className='backdrop-blur-2xl h-[40rem] mx-12 md:w-[60rem] w-full md:backdrop-blur-none'>
+			<div className='backdrop-blur-2xl bg-oldlace mx-2 h-[40rem] md:h-[60rem] md:mx-12 md:w-[60rem] w-full md:backdrop-blur-non shadow-lg bg-opacity-70'>
 				<TwitterTimelineEmbed
 					screenName='anjeshbaranwal'
 					sourceType='timeline'
 					theme='light'
 					transparent
 					autoHeight
+					// options={{ width: 'full' }}
+					placeholder='loading latest tweets...'
 				/>
 			</div>
 		</div>
