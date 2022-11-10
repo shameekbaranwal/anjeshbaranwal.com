bd58e17681f3918ca5985023827176d3e7bffe80
Shameek Kumar Baranwal <shameekbaranwal@gmail.com>
2022-11-11 01:47:51 +0530

    Removed kokila from Introduction.

--- src/components/Hero/Introduction.jsx
+++ src/components/Hero/Introduction.jsx
@@ -14,7 +14,8 @@ export default function Introduction() {
 				alt='quotes'
 				className='relative self-start w-12 top-5 -left-10 lg:top-6 lg:-left-12'
 			/>
-			<p className='font-kruti-bold-italic text-3xl lg:text-3xl text-center lg:text-justify w-[22rem] md:w-[40rem] lg:w-[25rem]'>
+			<p className='font-kokilab italic text-3xl lg:text-3xl text-center lg:text-justify w-[22rem] md:w-[40rem] lg:w-[25rem]'>
+				{/* <p className='font-kruti-bold-italic text-3xl lg:text-3xl text-center lg:text-justify w-[22rem] md:w-[40rem] lg:w-[25rem]'> */}
 				{Hindi.Introduction}
 			</p>
 			<img
@@ -25,7 +26,7 @@ export default function Introduction() {
 			<img
 				src={Signature}
 				alt='anjesh'
-				className='self-end w-48 -mt-12 md:w-36'
+				className='self-end w-48 -mt-8 md:w-36'
 			/>
 			<BuyNavDropdown>
 				<Menu.Button className='text-center text-2xl rounded-lg w-full max-w-md mt-4 text-oldlace font-bold py-2 buy-button hover:scale-[1.05] transition duration-100 active:opacity-20 '>
