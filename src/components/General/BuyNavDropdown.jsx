d231b4b690ff741c0fef8e965d957555e045a763
Shameek <shameekbaranwal@gmail.com>
2023-03-09 16:17:31 +0530

    Added Flipkart link.

--- src/components/General/BuyNavDropdown.jsx
+++ src/components/General/BuyNavDropdown.jsx
@@ -1,48 +1,55 @@
-import { Fragment } from 'react';
-import { Menu, Transition } from '@headlessui/react';
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
 
-import BuyOption from './BuyOption';
-import amazonImage from '../../assets/images/amazon.png';
-import garudaImage from '../../assets/images/garuda.png';
-import padhegaIndiaImage from '../../assets/images/padhegaindia.png';
+import BuyOption from "./BuyOption";
+import amazonImage from "../../assets/images/amazon.png";
+import garudaImage from "../../assets/images/garuda.png";
+import flipkartImage from "../../assets/images/flipkart.png";
+import padhegaIndiaImage from "../../assets/images/padhegaindia.png";
 
 export default function BuyNavDropdown({ children }) {
-	return (
-		<Menu as='div' className='relative inline-block w-full text-left'>
-			<div className='w-full'>{children}</div>
+  return (
+    <Menu as="div" className="relative inline-block w-full text-left">
+      <div className="w-full">{children}</div>
 
-			<Transition
-				as={Fragment}
-				enter='transition ease-out duration-100'
-				enterFrom='transform opacity-0 scale-95'
-				enterTo='transform opacity-100 scale-100'
-				leave='transition ease-in duration-75'
-				leaveFrom='transform opacity-100 scale-100'
-				leaveTo='transform opacity-0 scale-95'
-			>
-				<Menu.Items className='absolute right-0 z-10 px-4 py-2 mt-2 origin-top-right rounded-md shadow-lg w-72 bg-platinum focus:outline-none gap-y-4'>
-					<div className=''>
-						<BuyOption
-							img={padhegaIndiaImage}
-							name={'Padhega India'}
-							className='my-2.5 bg-hotgreen'
-							href='http://rzp.io/l/Sangachadwam'
-						/>
-						<BuyOption
-							img={garudaImage}
-							name={'Garuda Prakashan'}
-							className='my-2.5 bg-hotred'
-							href='https://garudabooks.com/sangachchhadhvam-sindhu-tat-ka-anaam-gaaon'
-						/>
-						<BuyOption
-							img={amazonImage}
-							name={'Amazon'}
-							className='my-2.5 bg-hotorange'
-							href='https://amzn.eu/d/g5MKH5J'
-						/>
-					</div>
-				</Menu.Items>
-			</Transition>
-		</Menu>
-	);
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 px-4 py-2 mt-2 origin-top-right rounded-md shadow-lg w-72 bg-platinum focus:outline-none gap-y-4">
+          <div className="">
+            <BuyOption
+              img={padhegaIndiaImage}
+              name={"Padhega India"}
+              className="my-2.5 bg-hotgreen"
+              href="http://rzp.io/l/Sangachadwam"
+            />
+            <BuyOption
+              img={garudaImage}
+              name={"Garuda Prakashan"}
+              className="my-2.5 bg-hotred"
+              href="https://garudabooks.com/sangachchhadhvam-sindhu-tat-ka-anaam-gaaon"
+            />
+            <BuyOption
+              img={amazonImage}
+              name={"Amazon"}
+              className="my-2.5 bg-hotorange"
+              href="https://amzn.eu/d/g5MKH5J"
+            />
+            <BuyOption
+              img={flipkartImage}
+              name={"Flipkart"}
+              className="my-2.5 bg-hotblue"
+              href="https://dl.flipkart.com/s/hOZsyquuuN"
+            />
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
 }
--- src/components/NavBar/VerticalNavOptions.jsx
+++ src/components/NavBar/VerticalNavOptions.jsx
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import NavOption from './NavOption';
-import SocialMedia from './SocialMedia';
-import Hindi from '../../constants/Hindi.js';
-import BuyVerticalNav from './BuyVerticalNav';
+import NavOption from "./NavOption";
+import SocialMedia from "./SocialMedia";
+import Hindi from "../../constants/Hindi.js";
+import BuyVerticalNav from "./BuyVerticalNav";
 
 export default function VerticalNavOptions({ isOpen, setIsOpen }) {
-	return (
-		<ul
-			className={`fixed md:hidden overflow-hidden bg-platinum shadow-md rounded flex flex-col items-center overflow-x-hidden transition-all duration-500 text-brown-100 w-full z-10 gap-y-6 ${
-				isOpen ? 'h-[100%] pt-20' : 'h-[0%] pt-14 list-none'
-			}`}
-		>
-			<NavOption onClick={() => setIsOpen(false)} href='./#Reviews'>
-				{Hindi.Reviews1} / {Hindi.Reviews2}
-			</NavOption>
-			<NavOption onClick={() => setIsOpen(false)} href='./#Foreword'>
-				{Hindi.Foreword}
-			</NavOption>
-			<NavOption onClick={() => setIsOpen(false)} href='./#Preface'>
-				{Hindi.Preface}
-			</NavOption>
-			<NavOption onClick={() => setIsOpen(false)} href='./#Author'>
-				{Hindi.Author}
-			</NavOption>
+  return (
+    <ul
+      className={`fixed md:hidden overflow-hidden bg-platinum shadow-md rounded flex flex-col items-center overflow-x-hidden transition-all duration-500 text-brown-100 w-full z-10 gap-y-6 ${
+        isOpen ? "h-[100%] pt-20" : "h-[0%] pt-14 list-none"
+      }`}
+    >
+      <NavOption onClick={() => setIsOpen(false)} href="./#Reviews">
+        {Hindi.Reviews1} / {Hindi.Reviews2}
+      </NavOption>
+      <NavOption onClick={() => setIsOpen(false)} href="./#Foreword">
+        {Hindi.Foreword}
+      </NavOption>
+      <NavOption onClick={() => setIsOpen(false)} href="./#Preface">
+        {Hindi.Preface}
+      </NavOption>
+      <NavOption onClick={() => setIsOpen(false)} href="./#Author">
+        {Hindi.Author}
+      </NavOption>
 
-			<BuyVerticalNav closeNavBar={() => setIsOpen(false)} />
+      <BuyVerticalNav closeNavBar={() => setIsOpen(false)} />
 
-			<SocialMedia
-				onClick={() => setIsOpen(false)}
-				className='mt-12 gap-x-8'
-			/>
-		</ul>
-	);
+      <SocialMedia
+        onClick={() => setIsOpen(false)}
+        className="mt-4 gap-x-8"
+      />
+    </ul>
+  );
 }
